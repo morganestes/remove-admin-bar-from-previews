20c2c7546210874822f25a710a7ebd169b7e3195
Morgan Estes <morgan@golftailor.com>
2014-05-13 22:31:47 -0500

    Add notification bar reminder option.

--- admin-bar-remove-preview.php
+++ admin-bar-remove-preview.php
@@ -16,8 +16,14 @@
  */
 function abp_hide_admin_bar() {
 	$options = get_option( 'abp_hidden' );
-	if ( is_preview() && true == $options['hide-on-preview'] ) {
-		add_filter( 'show_admin_bar', '__return_false' );
+	if ( is_preview() ) {
+		if ( true == $options['hide-on-preview'] ) {
+			add_filter( 'show_admin_bar', '__return_false' );
+		}
+		if ( true == $options['show-notice-bar'] ) {
+			add_action( 'wp_enqueue_scripts', 'abp_notice_bar_style' );
+			add_filter( 'wp_footer', 'abp_notice_bar' );
+		}
 	}
 }
 
@@ -30,6 +36,7 @@ function abp_settings_api_init() {
 	if ( false === get_option( 'abp_hidden' ) ) {
 		$defaults = array(
 			'hide-on-preview' => true,
+			'show-notice-bar' => true,
 		);
 		update_option( 'abp_hidden', $defaults );
 	}
@@ -50,6 +57,7 @@ add_action( 'admin_init', 'abp_settings_api_init' );
  */
 function abp_check_options( $options ) {
 	$options['hide-on-preview'] = intval( $options['hide-on-preview'] );
+	$options['show-notice-bar'] = intval( $options['show-notice-bar'] );
 
 	return $options;
 }
@@ -62,5 +70,23 @@ function abp_settings_field() {
 	?>
 	<input type="checkbox" name="abp_hidden[hide-on-preview]" id="abp-post-preview" value="1" <?php checked( $options['hide-on-preview'], 1 ); ?> />
 	<label for="abp-post-preview"><?php _e( 'Hide the Admin Bar when previewing posts and pages', 'abp-hidden' ); ?></label>
+	<br>
+	<input type="checkbox" name="abp_hidden[show-notice-bar]" id="abp-show-notice" value="1" <?php checked( $options['show-notice-bar'], 1 ); ?> />
+	<label for="abp-show-notice"><?php _e( 'Show a reminder notice when previewing a post', 'abp-hidden' ); ?></label>
 <?php
 }
+
+function abp_notice_bar( ) {
+$notice_bar = <<<HTML
+<div class="abp-notice">
+<p>This is a preview, and changes may not have been saved.</p>
+</div>
+HTML;
+
+	echo $notice_bar;
+}
+
+function abp_notice_bar_style() {
+	wp_enqueue_style( 'abp-preview-notice', plugins_url( '/preview-notice.css', __FILE__ ) );
+}
+
