c69abf12b2eac7280724201fc7ad7696c9fcb0d8
Morgan Estes <morgan@golftailor.com>
2014-05-14 13:07:28 -0500

    Changed option names and added a notification bar option.

--- admin-bar-remove-preview.php
+++ admin-bar-remove-preview.php
@@ -15,12 +15,12 @@
  * @return bool|void
  */
 function abp_hide_admin_bar() {
-	$options = get_option( 'abp_hidden' );
+	$options = get_option( 'abp_preview_settings' );
 	if ( is_preview() ) {
-		if ( true == $options['hide-on-preview'] ) {
+		if ( true == $options['hide_on_preview'] ) {
 			add_filter( 'show_admin_bar', '__return_false' );
 		}
-		if ( true == $options['show-notice-bar'] ) {
+		if ( true == $options['show_notice_bar'] ) {
 			add_action( 'wp_enqueue_scripts', 'abp_notice_bar_style' );
 			add_filter( 'wp_footer', 'abp_notice_bar' );
 		}
@@ -33,17 +33,17 @@ add_action( 'wp', 'abp_hide_admin_bar' );
  * Create the settings fields and register them with the Admin UI.
  */
 function abp_settings_api_init() {
-	if ( false === get_option( 'abp_hidden' ) ) {
+	if ( false === get_option( 'abp_preview_settings' ) ) {
 		$defaults = array(
-			'hide-on-preview' => true,
-			'show-notice-bar' => true,
+			'hide_on_preview' => 1,
+			'show_notice_bar' => 1,
 		);
-		update_option( 'abp_hidden', $defaults );
+		update_option( 'abp_preview_settings', $defaults );
 	}
 
 	add_settings_field( 'abp-post-preview', 'Hide Admin Bar on Preview', 'abp_settings_field', 'writing', 'default' );
 
-	register_setting( 'writing', 'abp_hidden', 'abp_check_options' );
+	register_setting( 'writing', 'abp_preview_settings', 'abp_check_options' );
 }
 
 add_action( 'admin_init', 'abp_settings_api_init' );
@@ -51,13 +51,15 @@ add_action( 'admin_init', 'abp_settings_api_init' );
 /**
  * Filter and validate the options.
  *
- * @param $options
+ * @param $options Serialized string set by *_option().
  *
- * @return array
+ * @return array The filtered options.
  */
 function abp_check_options( $options ) {
-	$options['hide-on-preview'] = intval( $options['hide-on-preview'] );
-	$options['show-notice-bar'] = intval( $options['show-notice-bar'] );
+	// Use intval() instead of boolval() for checkboxes
+	// @link http://wptheming.com/2011/05/checkboxes-and-booleans/
+	$options['hide_on_preview'] = intval( $options['hide_on_preview'] );
+	$options['show_notice_bar'] = intval( $options['show_notice_bar'] );
 
 	return $options;
 }
@@ -66,27 +68,24 @@ function abp_check_options( $options ) {
  * Create the settings fields HTML in the Writing admin page.
  */
 function abp_settings_field() {
-	$options = get_option( 'abp_hidden' );
+	$options = get_option( 'abp_preview_settings' );
 	?>
-	<input type="checkbox" name="abp_hidden[hide-on-preview]" id="abp-post-preview" value="1" <?php checked( $options['hide-on-preview'], 1 ); ?> />
+	<input type="checkbox" name="abp_preview_settings[hide_on_preview]" id="abp-post-preview" value="1" <?php checked( $options['hide_on_preview'], 1 ); ?> />
 	<label for="abp-post-preview"><?php _e( 'Hide the Admin Bar when previewing posts and pages', 'abp-hidden' ); ?></label>
 	<br>
-	<input type="checkbox" name="abp_hidden[show-notice-bar]" id="abp-show-notice" value="1" <?php checked( $options['show-notice-bar'], 1 ); ?> />
+	<input type="checkbox" name="abp_preview_settings[show_notice_bar]" id="abp-show-notice" value="1" <?php checked( $options['show_notice_bar'], 1 ); ?> />
 	<label for="abp-show-notice"><?php _e( 'Show a reminder notice when previewing a post', 'abp-hidden' ); ?></label>
 <?php
 }
 
-function abp_notice_bar( ) {
-$notice_bar = <<<HTML
-<div class="abp-notice">
-<p>This is a preview, and changes may not have been saved.</p>
-</div>
-HTML;
-
-	echo $notice_bar;
+function abp_notice_bar() {
+	?>
+	<div class="abp-notice">
+		<p><?php _e( 'This is a preview, and changes may not have been saved.', 'abp-hidden' ); ?></p>
+	</div>
+<?php
 }
 
 function abp_notice_bar_style() {
 	wp_enqueue_style( 'abp-preview-notice', plugins_url( '/preview-notice.css', __FILE__ ) );
 }
-
